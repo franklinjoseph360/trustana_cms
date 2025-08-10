7086178c5a153ec8fa99709ae33eb6d73dbc1a70
Franklin Joseph <franklinjoseph360@gmail.com>
2025-08-11 01:41:05 +0530

    fix(trustana_cms): make /docs route aware of swagger

--- src/lambda.ts
+++ src/lambda.ts
@@ -1,3 +1,4 @@
+// src/lambda.ts
 import 'source-map-support/register'
 import 'reflect-metadata'
 import { Handler } from 'aws-lambda'
@@ -6,13 +7,37 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { ExpressAdapter } from '@nestjs/platform-express'
 import express from 'express'
+import { ValidationPipe } from '@nestjs/common'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 let cached: Handler | null = null
 
 async function bootstrap(): Promise<Handler> {
   const expressApp = express()
   const app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp), { bufferLogs: true })
+
   app.enableCors()
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: { enableImplicitConversion: true },
+    }),
+  )
+
+  const config = new DocumentBuilder()
+    .setTitle('Trustana - Attributes API')
+    .setVersion('1.0.0')
+    .build()
+
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('docs', app, document, {
+    useGlobalPrefix: false,
+    swaggerOptions: { persistAuthorization: true },
+  })
+
   await app.init()
   return serverlessExpress({ app: expressApp })
 }
