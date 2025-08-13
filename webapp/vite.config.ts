d3b3705a746ff33ced368da585a3c704451f6201
Franklin Joseph <franklinjoseph360@gmail.com>
2025-08-14 03:27:05 +0530

    fix(trustana_cms-webapp): path resolve issue fix

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -1,10 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   server: { port: 5173, proxy: {
       '/attributes': 'http://localhost:3000',
-    }, }
+    }, },
+  resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@pages': path.resolve(__dirname, 'src/pages'),
+      '@services': path.resolve(__dirname, 'src/services'),
+      '@store': path.resolve(__dirname, 'src/store'),
+    },
+    // optional: add common extensions Vite should resolve
+    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json'],
+  },
 })
